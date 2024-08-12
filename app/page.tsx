34c4613a254e9120ae9cd07c61b99389c72555cc
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-12 15:44:58 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ export default function Home() {
         <h1 className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">
           WEBSITE NEW MENCINTAI DIRI SENDIRI!
         </h1>
+        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
+          Release Website On Ulang Tahun Bapak Saya!
+        </h2>
       </div>
       <NewsletterForm />
       <Socials />
